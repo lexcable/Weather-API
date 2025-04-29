33ca0b26c14608b63def624096877f15a8ce484d
Joseph Ndungu <jothefu@gmail.com>
2025-04-29 18:03:15 +0300

    Commit tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,12 @@
-import rippleui from 'rippleui';
-
-const tailwindConfig = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/rippleui/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/rippleui/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [rippleui],
-}
-
-export default tailwindConfig;
+  plugins: [require('rippleui')],
+};
